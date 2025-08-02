966edc8edfd3c2d3ea32e416edba9b92663356ec
Shanaa-yasmin <shanshana.kt@gmail.com>
2025-08-02 12:40:49 +0530

    Path Sum(tree)

--- leet112.py
+++ leet112.py
@@ -12,3 +12,24 @@ class Solution:
             return targetSum==root.val
         remsum=targetSum-root.val
         return (self.hasPathSum(root.left,remsum) or self.hasPathSum(root.right,remsum))
+
+
+OR
+
+
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        def dfs(node, currentSum):
+            if not node:
+                return False
+
+            currentSum += node.val
+
+            if not node.left and not node.right:
+                return currentSum == targetSum
+
+            left = dfs(node.left, currentSum)
+            right = dfs(node.right, currentSum)
+            return left or right
+
+        return dfs(root, 0) if root else False
